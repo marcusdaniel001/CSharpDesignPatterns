9a37a33898e4cb03cd94b3f3b6a30f0f62264950
Marcus <marcus.silva@nodis.com.br>
2022-08-10 22:15:07 -0300

    DesignPattern - Strategy

---
+++ CursoDesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CursoDesignPatterns;
+
+Imposto iss = new ISS();
+Imposto icms = new ICMS();
+
+Orcamento orcamento = new(500.0);
+
+CalculadorDeImpostos calculador = new();
+
+calculador.RealizaCalculo(orcamento, iss);
+calculador.RealizaCalculo(orcamento, icms);
